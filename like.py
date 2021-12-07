9a354abf15cd3668848f006ccc66b42e4434bc88
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2021-12-07 21:43:32 +0530

    now takes filename as arg

--- like.py
+++ like.py
@@ -2,10 +2,14 @@ from lark import Lark
 
 from eval import LikeEvaluator
 
+import sys
+
+filename = sys.argv[1]
+
 with open("like.lark") as f:
     lark_parser = Lark(f.read(), start="start", parser="lalr")
 
-with open("like.ll") as f:
+with open(filename) as f:
     lark_lang = f.read()
 
 ast = lark_parser.parse(lark_lang)
