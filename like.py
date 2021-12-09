0b78c5673db70cbc692580c96ded82582817b7fa
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2021-12-09 16:30:07 +0530

    chore: move error handling from like.py to it's own file

--- like.py
+++ like.py
@@ -1,68 +1,12 @@
 import sys
 
-from colorama import Back, Fore, Style, init
 from lark import Lark
 from lark.exceptions import UnexpectedCharacters, UnexpectedInput, VisitError
 
 from like.eval import LikeEvaluator, LikeSyntaxError
-
-init()
+from like.errors import syntax_error, runtime_error, get_context
 
 filename = sys.argv[1]
-
-
-def error_print(type: str, s: str):
-    print(f"{Fore.RED}{type}:{Style.RESET_ALL} {s}")
-
-
-def syntax_error(s: str):
-    error_print("Syntax Error", s)
-
-
-def runtime_error(s: str):
-    error_print("Runtime Error", s)
-
-
-def get_context(u: UnexpectedInput, text: str):
-    pos = u.pos_in_stream
-
-    start = pos
-
-    while start >= 0:
-        if text[start] == "\n":
-            break
-        start -= 1
-
-    if start == -1:
-        start += 1
-
-    # if text[start] == "\n":
-    #     start -= 1
-
-    end = pos
-
-    while end < len(text):
-        if text[end] == "\n":
-            break
-        end += 1
-
-    if end == len(text):
-        end -= 1
-
-    # if text[end] == "\n":
-    #     end += 1
-
-    line = text[start:end]
-
-    s = line.expandtabs()
-
-    err = (
-        f"{Fore.BLACK}{Back.WHITE}{s}{Style.RESET_ALL}\n"
-        + " " * (len(text[start:pos].expandtabs()) - 1)
-        + f"{Fore.BLUE}^{Style.RESET_ALL}"
-    )
-
-    return err
 
 
 with open("like.lark") as f:
@@ -72,7 +16,6 @@ with open(filename) as f:
     inp_str = f.read()
 try:
     ast = lark_parser.parse(inp_str)
-    # print(ast.pretty())
     res = LikeEvaluator().transform(ast)
 except VisitError as ve:
     if isinstance(ve.orig_exc, LikeSyntaxError):
@@ -82,8 +25,6 @@ except VisitError as ve:
 except UnexpectedCharacters as u:
     print(get_context(u, inp_str))
     syntax_error(f"Unknown character at line {u.line}, column {u.column}")
-    # print(u.get_context(inp_str))
 except UnexpectedInput as u:
     print(get_context(u, inp_str))
     syntax_error(f"at line {u.line}, column {u.column}")
-    # print(u.get_context(inp_str))
